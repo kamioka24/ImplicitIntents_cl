e4ae9f18ef0b15304da2f6b4c50bdf2b27cea7a2
kamioka24 <keee2404@gmail.com>
2021-08-07 09:23:36 +0900

    [Add]

--- app/src/main/java/com/example/implicitintents_cl/MainActivity.java
+++ app/src/main/java/com/example/implicitintents_cl/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.implicitintents_cl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.edit_text1);
+        mLocationEditText = findViewById(R.id.edit_text2);
+        mShareEditText = findViewById(R.id.edit_text3);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+
+        // 上の文字列を Uri オブジェクトにエンコードして解析する。
+        Uri webpage = Uri.parse(url);
+
+        // 指定されたデータの表示
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
     }
 }
