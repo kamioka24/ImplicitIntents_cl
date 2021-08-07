e5bdbfdc4256e0d6f891828ea6d8328bab247f17
kamioka24 <keee2404@gmail.com>
2021-08-07 10:30:50 +0900

    [Break]

--- app/src/main/java/com/example/implicitintents_cl/MainActivity.java
+++ app/src/main/java/com/example/implicitintents_cl/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.implicitintents_cl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         mShareEditText = findViewById(R.id.edit_text3);
     }
 
+    // web サイトを開くメソッド
     public void openWebsite(View view) {
         String url = mWebsiteEditText.getText().toString();
 
@@ -33,5 +36,43 @@ public class MainActivity extends AppCompatActivity {
 
         // 指定されたデータの表示
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // null チェック。リクエスト処理できるものが、1つはあることを示す。
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("MainActivity", "webサイトの取得に失敗しました。");
+        }
+    }
+
+    // 場所を開くメソッド
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        // 地理検索クエリを使用してその文字列を Uri オブジェクトに解析する。
+        Uri addressUri = Uri.parse("geo:0.0?q=" + loc);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("MainActivity", "場所の取得に失敗しました。");
+        }
+    }
+
+    // 共有ボタン処理
+    public void shareText(View view) {
+        String txt = mShareEditText.getText().toString();
+
+        // 共有する mime タイプを定義する。
+        String mimeType = "text/plain";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
     }
 }
